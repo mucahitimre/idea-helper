ef7450169cc1e5f60751dc2d9e5537e1b2646a71
Mücahit imre <mucahit.imre@gmail.com>
2021-12-30 19:27:13 +0300

    Add project files.

---
+++ IdeaHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdeaHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
