d3ff397b4ae2edad145b4e933c0884e71a1837d8
Mücahit imre <mucahit.imre@gmail.com>
2022-02-05 17:16:12 +0300

    DynamicObject implementation example.

---
+++ IdeaHelper/DynamicObjectExample.cs
@@ -0,0 +1,48 @@
+﻿using System.Dynamic;
+
+namespace IdeaHelper
+{
+    // Reference: https://www.codeguru.com/csharp/using-dynamicobject-and-expandoobject/
+    public class DynamicObjectExample : DynamicObject
+    {
+        private readonly Dictionary<string, object> _properties = new();
+
+        // If Property is called, 'TryGetMember' is triggered.
+        public override bool TryGetMember(GetMemberBinder binder, out object? result)
+        {
+            if (_properties.ContainsKey(binder.Name))
+            {
+                result = _properties[binder.Name];
+                return true;
+            }
+            else
+            {
+                result = null;
+                return false;
+            }
+        }
+
+        // Common set method.
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _properties[binder.Name] = value;
+            return true;
+        }
+
+        // If Method is called, 'TryInvokeMember' is triggered.
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object? result)
+        {
+            try
+            {
+                dynamic member = _properties[binder.Name];
+                result = member();
+                return true;
+            }
+            catch (Exception)
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+}
--- IdeaHelper/Program.cs
+++ IdeaHelper/Program.cs
@@ -4,6 +4,22 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        DynamicObjectExample();
+    }
+
+    private static void DynamicObjectExample()
+    {
+        dynamic dynamicObj = new DynamicObjectExample();
+        static double getPiNumber() => 3.14;
+        dynamicObj.GetPiNumber = (Func<double>)getPiNumber;
+        dynamicObj.PiNumber = 3.14;
+
+        // If Method is called, 'TryInvokeMember' is triggered.
+        var pi = dynamicObj.GetPiNumber();
+
+        // If Property is called, 'TryGetMember' is triggered.
+        double piProp = dynamicObj.PiNumber;
+
+        Console.WriteLine($"Hello World! {pi}");
     }
 }
